7addd3da94f67c689392619d89052669057f4f2a
vladkampov <vladyslavkampov@gmail.com>
2020-02-13 15:42:23 +0200

    Init

---
+++ precache-manifest.61ca0928119d9f7c50095cff7c849d6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad1aa02808f6e2b80e948e5e06a9e98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b464488b1d7f89508867",
+    "url": "/static/css/main.f9902fd2.chunk.css"
+  },
+  {
+    "revision": "237b051cadd26eeeccc9",
+    "url": "/static/js/2.dfedf3e6.chunk.js"
+  },
+  {
+    "revision": "b464488b1d7f89508867",
+    "url": "/static/js/main.a999cca9.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
